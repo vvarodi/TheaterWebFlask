32e932e24aff0189b7aab3639c3ccec6b2849cc4
vvarodi <vikivv2000@gmail.com>
2021-12-08 15:32:08 +0100

    before ajax

--- cinema/main.py
+++ cinema/main.py
@@ -1,7 +1,7 @@
 from datetime import date
 import dateutil.tz
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from . import model
 import flask_login
 
@@ -32,16 +32,48 @@ def movie(id):
     return render_template("movie.html", movie=movie, projections=projections)
 
 
+@bp.route("/user")
+@flask_login.login_required
+def user():
+    return render_template('user.html')
 
+
+@bp.route("/reservation/", defaults={'id': None})
 @bp.route("/reservation/<int:id>")
 @flask_login.login_required
 def reservation(id):
-    projection = model.Projection.query.get(id)
-    projections = model.Projection.query.filter(model.Projection.id == id).all()
-    return render_template("reservation.html", projection=projection, projections=projections)
+    all_projections = model.Projection.query.all()
+    if id == None:
+        return render_template("reservation.html", projection=None, projections=all_projections, all_projections=all_projections)
+    else:
+        projection = model.Projection.query.get(id)
+        projections = model.Projection.query.filter(model.Projection.movie_id == projection.movie_id).all()
+        return render_template("reservation.html", projection=projection, projections=projections, all_projections=all_projections)
 
 
 @bp.route("/reservation/<int:id>", methods=["POST"])
 @flask_login.login_required
 def reservation_post(id):
+
+
     return render_template("reservation.html")
+
+
+
+
+
+
+
+
+# @bp.route('/view')
+# def show_view():
+#     datos = model.Projection.query.all()
+#     return render_template("view.html", datos=datos)
+
+# @bp.route('/ajax', methods=["GET", "POST"])             
+# def ajax():
+#     if request.method == "POST":
+#         search = request.form.get("projection")
+
+#         projection = model.Projection.query.get(int(search))
+#         return render_template("view.html", projection=projection)
--- cinema/static/js/main.js
+++ cinema/static/js/main.js
@@ -1,1 +1,38 @@
 
+
+// CLIENT SIDE
+var interactiveForm = function(){
+    var CProj = $('#projection option:selected').val();
+    // $('#movie') = 
+    console.log(CProj);
+
+    $.ajax({
+        url:"/ajax",
+        type:"POST",
+        dataType: "json",
+        success: function(response){
+            console.log(response)
+
+        },
+        error: function(error){
+            console.log(error);
+        },
+        });
+
+}   
+
+//     // $.ajax({
+//     //     type: "GET",
+//     //     url: "/reservation/" + Cproj}).done(function(data){
+//     //     $("#choosen").empty();
+//     //     // if($("#vcenter").hasClass("category-filter")){
+//     //     //   $("#datacenter").append(
+//     //     //     $("<option></option>").attr("value", "0").text(" --- ")
+//     //     //   );
+//     //     });
+// }
+
+$(function(){
+    interactiveForm();
+}
+)
